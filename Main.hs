5c4635a3caa84d6c6214a74ade5e88c0bdc0271b
Paweł <poler@pop-os.localdomain>
2020-09-17 12:44:02 +0200

    minor refactorization

--- Main.hs
+++ Main.hs
@@ -5,11 +5,7 @@ import Data.List
 import Data.Char (digitToInt)
 
 main = do
-    putStrLn $ show $ (numOfmatches [3,4,4,4] [6,5,3,4])
-    putStrLn $ show $ (numOfmatches [0, 5, 0, 3] [6,5,3,4])
-    putStrLn $ show $ (numOfmatches [3,4,6,4] [6,5,3,4])
-    putStrLn $ show $ (numOfmatches [4,3,5,5] [6,5,3,4])
-
+    putStrLn "Welcome to Mastermind!"
     result <- makeGuess getAllPossibleCodes 0 0
     putStrLn $ show result
 
@@ -31,10 +27,8 @@ askForInput x = do
 
 makeGuess :: [[Int]] -> Int -> Int -> IO Bool
 makeGuess x red white = do
-    putStrLn $ show (length x)
     let y = getPossibleCodes x red white
     putStrLn $ show (length y)
-
     putStrLn $ show (y !! 0)
     askForInput y
 
@@ -52,22 +46,19 @@ data Code = Code {
 
 getAllPossibleCodes :: [[Int]]
 getAllPossibleCodes = do
-    a <- [0..6]
-    b <- [0..6]
-    c <- [0..6]
-    d <- [0..6]
-    [[a,b,c,d]]
+    a <- [1..6]
+    b <- [1..6]
+    c <- [1..6]
+    d <- [1..6]
+    [0,0,0,0] : [[a,b,c,d]]
 
 
 getPossibleCodes :: [[Int]] -> Int -> Int -> [[Int]]
 getPossibleCodes (x:xs) red white = do 
     let currentIt = (maximum x)
     [y | y <- xs, (getCount y currentIt) == red + white, (numOfmatches y x) == red]
-    --if (red == 0)
-    --else [y|  y <- xs, (getCount y currentIt) == red + white]
 
 numOfmatches :: [Int] -> [Int] -> Int
---numOfmatches x y = length [z | z <- x, x !! 0 == y !! 0 || x !! 1 == y !! 1 || x !! 2 == y !! 2 || x !! 3 == y !! 3 ]
 numOfmatches x y = do 
     let q = if x !! 0 == y !! 0 then 1 else 0
     let w = if x !! 1 == y !! 1 then 1 else 0
